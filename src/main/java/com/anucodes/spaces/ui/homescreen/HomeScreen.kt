0b6bb64a07e31ec5e60f7f52c255ac1a78a19411
Anubhav Singh <86763919+Anubhav8176@users.noreply.github.com>
2024-10-09 23:31:38 +0530

    Main UI

--- src/main/java/com/anucodes/spaces/ui/homescreen/HomeScreen.kt
+++ src/main/java/com/anucodes/spaces/ui/homescreen/HomeScreen.kt
@@ -3,14 +3,24 @@ package com.anucodes.spaces.ui.homescreen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.anucodes.spaces.authentication.viewmodel.AuthViewmodel
@@ -18,53 +28,48 @@ import com.anucodes.spaces.navigation.BottomNavigationBar
 import com.anucodes.spaces.ui.theme.poppinsFam
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     authViewmodel: AuthViewmodel,
     navController: NavHostController,
     innerpadding: PaddingValues
 ){
-
-    authViewmodel.fetchCurrentUser()
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(innerpadding),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ){
-        Text(
-            text = "This is home Screen",
-            fontSize = 30.sp,
-            fontFamily = poppinsFam
-        )
-        Button(
-            onClick = {
-                authViewmodel.logout()
-                navController.navigate("auth_graph"){
-                    popUpTo(navController.graph.startDestinationId){
-                        inclusive=true
-                    }
-                }
+    Column(modifier = Modifier.padding(innerpadding)){
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            text = "Spaces",
+                            fontSize = 40.sp,
+                            fontFamily = poppinsFam,
+                            textAlign = TextAlign.Center
+                        )
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.DarkGray,
+                        titleContentColor = Color.White
+                    )
+                )
             }
-        ) {
-            Text(
-                text = "Logout!",
-                fontSize = 16.sp,
-                fontFamily = poppinsFam
-            )
-        }
-        Button(
-            onClick = {
-                authViewmodel.fetchCurrentUser()
+        ){ innerpadding->
+            Column(
+                modifier = Modifier
+                    .padding(innerpadding)
+            ){
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    text = "Chats",
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    fontFamily = poppinsFam
+                )
             }
-        ) {
-            Text(
-                text = "Fetch User",
-                fontSize = 16.sp,
-                fontFamily = poppinsFam
-            )
         }
     }
 }
